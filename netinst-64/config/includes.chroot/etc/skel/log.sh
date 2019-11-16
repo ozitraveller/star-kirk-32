498759806db1652c3b579412faff9b9d4f671b66
ozitraveller <ozitraveller@gmail.com>
2019-11-17 08:11:49 +1100

    version 2.1.0

--- netinst-64/config/includes.chroot/etc/skel/log.sh
+++ netinst-64/config/includes.chroot/etc/skel/log.sh
@@ -12,8 +12,4 @@ tail -100  /var/log/Xorg.0.log >> x0log
 
 dmesg | tail -100 >> dlog
 
-# /usr/share/X11/xorg.cong.d
 
-# debian (ee) failed to load module vesa (module does not exist 0)
-# No drivers available
-# No screens found (EE)
